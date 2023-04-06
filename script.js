8be1f4e13ed7cc8b3acc16eab26b8ca534a9a994
Sudeep Shrestha <>
2023-04-06 08:21:46 +0000

    fetched data is displaying in card

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let url ='https://kontests.net/api/v1/all'
+let response = fetch(url)
+response.then((v)=>{
+  return v.json()
+}).then((contests)=>{
+  console.log(contests)
+  ihtml =""
+  for (item in contests){
+    console.log(contests[item])
+    ihtml += `
+         <div class="card d-flex" style="width: 18rem;">
+         
+         <div class="card-body">
+           <h5 class="card-tittle">${contests[item].name}.</h5>
+           <p> ${contests[item].start_time}</p>
+           <p> ${contests[item].end_time}</p>
+           <a href="${contests[item].url}  " class="btn btn-primary">Go to the Site</a>
+          
+         </div>
+       </div>
+       `
+  }
+cardContainer.innerHTML = ihtml
+})
+
+
+
+
